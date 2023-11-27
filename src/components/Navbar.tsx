adb140692d6067c1d542cbb1843f4326c8d34d17
d-zucho <danny.nico.dev@outlook.com>
2023-11-27 04:50:28 -0800

    add nav and interactivity

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,35 @@
-import Link from "next/link"
-import MaxWidthWrapper from "./MaxWidthWrapper"
-import { Icons } from "./Icons"
-import NavItems from "./NavItems"
+import Link from 'next/link';
+import MaxWidthWrapper from './MaxWidthWrapper';
+import { Icons } from './Icons';
+import NavItems from './NavItems';
 
 const Navbar = () => {
-  return (
-    <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
-        <header className="relative bg-white">
-            <MaxWidthWrapper>
-                <div className="border-b border-gray-200">
-                    <div className="flex h-16 items-center">
+	return (
+		<div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
+			<header className='relative bg-white'>
+				<MaxWidthWrapper>
+					<div className='border-b border-gray-200'>
+						<div className='flex h-16 items-center'>
+							{/* TODO: Mobile Nav */}
+							<div className='ml-4 flex kg:ml-0'>
+								<Link href='/'>
+									<Icons.logo className='h-10 w-10' />
+								</Link>
+							</div>
 
-                        {/* TODO: Mobile Nav */}
-                        <div className="ml-4 flex kg:ml-0">
-                            <Link href='/'>
-                                <Icons.logo className="h-10 w-10" />
-                            </Link>
-                        </div>
+							<div className='hidden z-50 lg:ml-8 lg:block lg:self-stretch'>
+								<NavItems />
+							</div>
 
-                        <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
-                            <NavItems />
-                        </div>
-                    </div>
-                </div>
-            </MaxWidthWrapper>
-        </header>
-    </div>
-  )
-}
+							<div className='ml-auto flex items-center'>
+								<div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'></div>
+							</div>
+						</div>
+					</div>
+				</MaxWidthWrapper>
+			</header>
+		</div>
+	);
+};
 
-export default Navbar
+export default Navbar;
