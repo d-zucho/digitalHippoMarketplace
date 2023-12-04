5daae5862c81453a7e86f57fcf8a7a7247effa9b
D_Zucho <danny.nico.dev@outlook.com>
2023-12-04 09:55:17 -0800

    add cart side modal on click

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -8,8 +8,11 @@ import {
   SheetTitle,
   SheetTrigger,
 } from './ui/sheet'
+import { Separator } from './ui/separator'
+import { formatPrice } from '@/lib/utils'
 
 const Cart = () => {
+  const itemCount = 2
   return (
     <Sheet>
       {/* adding group to the SheetTrigger element, will allow us to add a trigger effect that is
@@ -20,13 +23,38 @@ const Cart = () => {
           aria-hidden='true'
         />
         <span className='ml2 - text-sm font-medium text-gray-700 group-hover:text-gray-800'>
-          0
+          {itemCount}
         </span>
       </SheetTrigger>
       <SheetContent className='flex w-full flex-col pr00 sm:max-w-lg'>
         <SheetHeader className='space-y-2.5 pr-6'>
-          <SheetTitle>Cart (0)</SheetTitle>
+          <SheetTitle>Cart ({itemCount})</SheetTitle>
         </SheetHeader>
+        {itemCount > 0 ? (
+          <>
+            <div className='flex w-full flex-col pr-6'>
+              {/* TODO: cart logic */}
+              cart items
+            </div>
+            <div className='space-y-4 pr-6'>
+              <Separator />
+              <div className='space-y-1.5 text-sm'>
+                <div className='flex'>
+                  <span className='flex-1'>Shipping</span>
+                  <span>Free</span>
+                </div>
+              </div>
+              <div className='space-y-1.5 text-sm'>
+                <div className='flex'>
+                  <span className='flex-1'>Transaction Fee</span>
+                  <span>{formatPrice(1)}</span>
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div></div>
+        )}
       </SheetContent>
     </Sheet>
   )
